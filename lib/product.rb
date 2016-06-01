63183da07bf7b227b56374e2098d425ac2b17b25
Jamie Moreland <jamiemorelanduk@gmail.com>
2016-06-01 15:02:21 +0100

    Added DuplicateProductError error

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,25 @@
 class Product
+    attr_reader :title
+
     @@products = []
+
     def initialize(options={})
-        @@products << self
+        @title = options[:title]
+        add_to_products
     end
 
     def self.all
         @@products
+    end
+
+    private
+
+    def add_to_products
+        @@products.each do |item|
+            if self.title == item.title
+                raise DuplicateProductError, "#{self.title} already exists."
+            end
+        end
+        @@products << self
     end
 end
